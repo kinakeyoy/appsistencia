16b21d0be69346b45574709440c03a4b3121bc29
Enrique Rojas <erojasr@unicartagena.edu.co>
2017-04-22 23:34:30 -0500

    corrigiendo contraseña

--- datos.php
+++ datos.php
@@ -5,7 +5,7 @@
 //$Database="BDAppsistencia";
 //$conexion= mysqli_connect($host, $usuario, $clave, $base);
 try {
-    $conexion = new PDO("sqlsrv:server = tcp:serverbdappsistencia.database.windows.net,1433; Database = BDAppsistencia", "userbdasistencia", "{Sismad2017}");
+    $conexion = new PDO("sqlsrv:server = tcp:serverbdappsistencia.database.windows.net,1433; Database = BDAppsistencia", "userbdasistencia", "Sismad2017");
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
