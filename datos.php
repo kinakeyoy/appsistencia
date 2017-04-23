dc3f92fb7b1df5515aace7a5d04b6e1fc41fcf3a
Enrique Rojas <erojasr@unicartagena.edu.co>
2017-04-22 23:25:45 -0500

    nombre base de datos 4

--- datos.php
+++ datos.php
@@ -1,9 +1,11 @@
 <?php
-$host="serverbdappsistencia.database.windows.net,1433";
+$host="sqlsrv:server = tcp:serverbdappsistencia.database.windows.net,1433";
 $usuario="userbdasistencia";
 $clave="Sismad2017";
 $base="BDAppsistencia";
 $conexion= mysqli_connect($host, $usuario, $clave, $base);
 ?>
+
+
 
 
