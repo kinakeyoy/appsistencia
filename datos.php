811d5f61cf164a1f3a4eeffc36f7f748550ed659
Enrique Rojas <erojasr@unicartagena.edu.co>
2017-04-22 23:30:59 -0500

    nombre de base dato 5

--- datos.php
+++ datos.php
@@ -1,9 +1,18 @@
 <?php
-$host="sqlsrv:server = tcp:serverbdappsistencia.database.windows.net,1433";
-$usuario="userbdasistencia";
-$clave="Sismad2017";
-$base="BDAppsistencia";
-$conexion= mysqli_connect($host, $usuario, $clave, $base);
+//$host="sqlsrv:server = tcp:serverbdappsistencia.database.windows.net,1433";
+//$usuario="userbdasistencia";
+//$clave="Sismad2017";
+//$Database="BDAppsistencia";
+//$conexion= mysqli_connect($host, $usuario, $clave, $base);
+try {
+    $conexion = new PDO("sqlsrv:server = tcp:serverbdappsistencia.database.windows.net,1433; Database = BDAppsistencia", "userbdasistencia", "{your_password_here}");
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
 ?>
 
 
