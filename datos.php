d4560c642468dfd968d405264eec7cdf0dff2d24
Enrique Rojas <erojasr@unicartagena.edu.co>
2017-04-26 08:48:10 -0500

    corrieginedo variable conexion
    corrieindo variable conexion

--- datos.php
+++ datos.php
@@ -3,7 +3,7 @@ $host="us-cdbr-azure-east-c.cloudapp.net";
 $usuario="b049e02b7dcb0d";
 $clave="303abca4";
 $Database="bdappsistencia";
-$conexion= mysqli_connect($host, $usuario, $clave, $base);
+//$conexion= mysqli_connect($host, $usuario, $clave, $base);
  // Connect to database.
  try {
      $conexion = new PDO( "mysql:host=$host;dbname=$Database", $useario, $clave);
