06d10d179fee5208d1ed91aca1ab1ea7829a5b5e
Enrique Rojas <erojasr@unicartagena.edu.co>
2017-04-26 10:02:50 -0500

    corrigiendo cadena de conexion
    corrigiendo cadena de conexion

--- datos.php
+++ datos.php
@@ -3,15 +3,9 @@ $host="us-cdbr-azure-east-c.cloudapp.net";
 $usuario="b049e02b7dcb0d";
 $clave="303abca4";
 $Database="bdappsistencia";
-//$conexion= mysqli_connect($host, $usuario, $clave, $base);
- // Connect to database.
- try {
-     $conexion = new PDO( "mysql:host=$host;dbname=$Database", $useario, $clave);
-     $conexion->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
- }
- catch(Exception $e){
-     die(var_dump($e));
- }
+$conexion= mysqli_connect($host, $usuario, $clave, $base);
+ 
+ 
 
 ?>
 
