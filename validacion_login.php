db0dea0ec0a1515d851130ea8eed688b97079c16
Enrique Rojas Reyes <erojasr@unicartagena.edu.co>
2017-04-27 00:55:38 -0500

    modificando validacion despues de pasado la prueba anterior

--- validacion_login.php
+++ validacion_login.php
@@ -1,8 +1,8 @@
 <?php
 
 echo "dentro de archivo validacion";
-// incluimos lo datos de conexion y las funciones
-/*include("datos.php");
+//incluimos lo datos de conexion y las funciones
+include("datos.php");
 	$usuario=$_POST["fcodigo"];
 	$pass=$_POST["fpassword"];
 	$select=$_POST["fselect"];
@@ -55,50 +55,5 @@ mysqli_close()
 
 
 
-//$sql = "SELECT codigo_login,pass_login, id_login FROM login where codigo_login='".$usuario."' and pass_login='".$pass."' and id_login=".$select;
-//$result = mysqli_query($conexion, $sql);
-//$array=mysql_query(SELECT id_login FROM login,conexion);
-/*
-
-if ($result) {   
-    echo 'consulta Realizada';
-    echo "<h2> Se ha transferido el archivo $array["id_login"]</h2>";
-    //echo $pass_login;
-    //echo $id_login;
-    //	$_SESSION["user"]=$usuario; 	
-	//header('Location: /Semillero/estudiante.php');*/
-
-//else echo "Problema con la Consulta";
-
-
-//}
-//else("Connection Failed:".mysqli_connect_error());
-
-
-/*
-
-if (!$conexion) {
-    die("Connection failed: ".mysqli_connect_error());
-}
-
-$sql = "SELECT codigo_login,pass_login FROM login where codigo_login='".$usuario."' and pass_login='".$pass."' and id_login=".$select;
-$result = mysqli_query($conexion, $sql);
-
-if (mysqli_num_rows($result)==1 && $select==1) {
-   
-    echo 'credencial correcta';
-	$_SESSION["user"]=$usuario; 
-	header('Location: /Semillero/estudiante.php');
-}
-else
-  
-*/
-
-/* codigo bootstarp para una caja de texto
-<div class="alert alert-success">
-  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  <strong>Success!</strong> Indicates a successful or positive action.
-</div>
-*/
 
 ?>
