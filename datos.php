60fabf00fe32da43a1be466d018bed71b8faa4f0
Enrique Rojas Reyes <erojasr@unicartagena.edu.co>
2017-04-27 00:03:35 -0500

    comentarios para probar desde sublime 3

--- datos.php
+++ datos.php
@@ -6,6 +6,9 @@ $base="registro_asistencia_2";
 $conexion= mysqli_connect($host, $usuario, $clave, $base);
 
 //prueba de github desde sublime
+//prueba de github desde sublime
+//prueba de github desde sublime
+//prueba de github desde sublime
 ?>
 
 
