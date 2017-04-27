3d8c025f54df18f8ebaa40feb82c37931742ca9f
Enrique Rojas Reyes <erojasr@unicartagena.edu.co>
2017-04-27 00:38:30 -0500

    cmabiando uaurio cadena de conexion

--- datos.php
+++ datos.php
@@ -1,7 +1,7 @@
 <?php
 $host="us-cdbr-azure-east-c.cloudapp.net";
-$usuario="kinakeyoy";
-$clave="$Campo2028";
+$usuario="b049e02b7dcb0d";
+$clave="$303abca4";
 $Database="bdappsistencia";
 $conexion= mysqli_connect($host, $usuario, $clave, $base);
 <<<<<<< HEAD
